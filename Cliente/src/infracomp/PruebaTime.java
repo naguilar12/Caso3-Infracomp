8b7d7044c0fe5b582b6c8fb411a06103aad1d45c
naguilar12 <n.aguilar@uniandes.edu.co>
2017-11-11 19:36:25 -0500

    Cambios

--- Cliente/src/infracomp/PruebaTime.java
+++ Cliente/src/infracomp/PruebaTime.java
@@ -4,19 +4,19 @@ import java.util.Date;
 
 public class PruebaTime {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Date a = new Date();
-		System.out.println(a);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Date b = new Date();
-		System.out.println(b);
-		System.out.println(b.getTime()-a.getTime());
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Date a = new Date();
+//		System.out.println(a);
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		Date b = new Date();
+//		System.out.println(b);
+//		System.out.println(b.getTime()-a.getTime());
+//	}
 
 }
